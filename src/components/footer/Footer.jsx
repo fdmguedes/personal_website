b0b06d09818afd401b2c6278dc49225f4ddb9475
MeerKat <98255504+MeerKatPT@users.noreply.github.com>
2022-12-14 07:41:03 +0000

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import "./footer.css";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="/#" className="footer__logo">
+        Filipe Guedes
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="/#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Skills</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/filipe--guedes/">
+          <FaLinkedinIn />
+        </a>
+        <a href="https://github.com/MeerKatPT">
+          <FaGithub />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
